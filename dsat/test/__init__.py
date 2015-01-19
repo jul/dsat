5860b093686f79eeb054728fdf5a20a6c3eb5d9c
jul <jtayon@ubity.com>
2015-01-19 12:50:54 -0500

    minimal working version that is broken

--- dsat/test/__init__.py
+++ dsat/test/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 import unittest
-from dsat.test.test_state import TestFilterDict,TestState
+from dsat.test.test_state import TestFilterDict
 from dsat.test.test_st_send_parse import TestSendParse
 
 if __name__ == '__main__':
