fb7c73ef7508bfccbbac92937733abade1cf10d1
jul <jtayon@ubity.com>
2015-03-13 10:02:15 -0400

    option living_dangerously added to carbon maker:     if True when given as an arguments send will return a dummy function     if you cannot connect the socket the first time     thus code can keep carbon_send without being used.

--- dsat/carbon.py
+++ dsat/carbon.py
@@ -13,15 +13,21 @@ DEF_CFG=dict(
 def carbon_maker(ctx, **options):
     sock=sock = socket.socket()
     cfg = DEF_CFG.copy()
-    cfg.update(ctx.config)
-    cfg.update(ctx.local_info)
-    
-    sock.connect((cfg['carbon']['server'], cfg['carbon']['port']))
+    cfg.update(options)
+    try: 
+        sock.connect((cfg['carbon']['server'], cfg['carbon']['port']))
+    except Exception as e:
+        if cfg.get("living_dangerously", True):
+            print "Exception %r caught, desactivating carbon" % e
+            print "returning dummy function instead of sent"
+            return lambda *a, **kw: True
+        else:
+            raise e
 
     def send(measure_dict):
         if not measure_dict:
             return None
-        try_me = 2
+        try_me = 1
         timestp = int(time())
         message = '\n'.join(["%s %s %s" % (
             ".".join([ "DSAT",  cfg["name"], cfg["where"], cfg["step"], path]), 
@@ -35,13 +41,19 @@ def carbon_maker(ctx, **options):
             except Exception as last_excp:
                 ## TODO intercept only connection error
                 sock.close()
-                
                 sock.connect((cfg['carbon']['server'], cfg['carbon']['port']))
                 raise("%r for %r" % (last_excp, cfg))
             try_me -= 1
-            
         return not try_me
     return send
 
 
+if "__main__" == __name__:
+    carbon_maker(dict(carbon=dict(
+        host="127.0.0.1", port=2003, living_dangerously = True),
+        where = "here",
+        step = "test",
+        )
+    )(dict( path = 1))
+    print "ok"
 
